254399dd1450c99ae4c17df54affce2b73298676
modbots <77435462+modbots@users.noreply.github.com>
2023-01-25 22:51:40 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,4 +13,4 @@ Popen(f"gunicorn server.server:app --bind 0.0.0.0:8080", shell=True)
 
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling()
