bc8604ab2538f464bd8237e4de20b9cc934435ce
modbots <77435462+modbots@users.noreply.github.com>
2023-01-25 22:58:09 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,10 +2,13 @@ import logging
 import random
 import json
 from subprocess import Popen
+from aiogram import Bot, Dispatcher, executor, types
 
+
+
+TOKEN = '5792941350:AAH-4wZamDd5UCseeJh5xDjiafKDs8SY2a9'
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 Popen(f"gunicorn server.server:app --bind 0.0.0.0:8080", shell=True)
-
-
-
-
-   
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
