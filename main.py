f5ff2244ef3e9703def6d04b67dee312dccd2656
modbots <77435462+modbots@users.noreply.github.com>
2023-01-26 00:02:21 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from subprocess import Popen
-Popen(f"gunicorn server.server:app --bind 0.0.0.0:8080", shell=True)
+Popen(f"gunicorn bot:app --bind 0.0.0.0:8080", shell=True)
 #Popen("python3 bot.py", shell=True)
