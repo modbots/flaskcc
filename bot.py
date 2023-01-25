b06ac2fda0425c71cc27efe0bc4a1ad94f0c0fdf
modbots <77435462+modbots@users.noreply.github.com>
2023-01-25 22:49:55 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,10 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-import markups as nav
+
 import random
 import json
 
 from subprocess import Popen
-
-TOKEN = ''
-CHANNEL_ID = '@binbeginner'
-NOT_SUB = 'Subscribe to this channel first'
-ANOTHER_NOT_SUB = 'Subscription not found'
-
-logging.basicConfig(level=logging.INFO)
-
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
-
-#return True if user is subscribed
-def check_sub(chat_member):
-    if chat_member['status'] != 'left':
-        return True
-    else:
-        return False
 
 
 
