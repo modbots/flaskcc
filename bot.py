f254c3ab8f232ca7c4a1b78c1c7b853c223d434a
modbots <77435462+modbots@users.noreply.github.com>
2023-01-25 23:05:18 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,13 +2,14 @@ import logging
 import random
 import json
 from subprocess import Popen
-from aiogram import Bot, Dispatcher, executor, types
 
+def echo(text: str, repetitions: int = 3) -> str:
+    """Imitate a real-world echo."""
+    echoed_text = ""
+    for i in range(repetitions, 0, -1):
+        echoed_text += f"{text[-i:]}\n"
+    return f"{echoed_text.lower()}."
 Popen(f"gunicorn server.server:app --bind 0.0.0.0:8080", shell=True)
-
-TOKEN = '5792941350:AAH-4wZamDd5UCseeJh5xDjiafKDs8SY2a9'
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
-
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    text = "LEEE ALL"
+    print(echo(text))
