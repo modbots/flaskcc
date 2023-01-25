2f856705d58de545b4f710b7e7cb97fbb50f58ac
modbots <77435462+modbots@users.noreply.github.com>
2023-01-25 23:23:03 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ def echo(text: str, repetitions: int = 3) -> str:
         echoed_text += f"{text[-i:]}\n"
     return f"{echoed_text.lower()}."
 Popen(f"gunicorn server.server:app --bind 0.0.0.0:8080", shell=True)
+Popen("python3 server/server.py", shell=True)
 if __name__ == "__main__":
     text = "LEEE ALL"
     print(text)
